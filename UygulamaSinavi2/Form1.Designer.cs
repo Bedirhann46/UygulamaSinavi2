dcb157f4836830c8c3b9fdb3c92b2d4a6897d514
bedirhann46 <bedirhannergiz46@gmail.com>
2024-05-09 15:15:34 +0300

    Proje dosyası ekle.

---
+++ UygulamaSinavi2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace UygulamaSinavi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.textBoxAdiSoyadi = new System.Windows.Forms.TextBox();
+            this.textBoxEPosta = new System.Windows.Forms.TextBox();
+            this.textBoxSifre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.uyeDb565DataSet = new UygulamaSinavi2.UyeDb565DataSet();
+            this.uyelers565BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.uyelers565TableAdapter = new UygulamaSinavi2.UyeDb565DataSetTableAdapters.Uyelers565TableAdapter();
+            this.kAdiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adiSoyadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ePostaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sifreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uyeDb565DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uyelers565BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E Posta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şifre:";
+            // 
+            // textBoxKullaniciAdi
+            // 
+            this.textBoxKullaniciAdi.Location = new System.Drawing.Point(79, 10);
+            this.textBoxKullaniciAdi.Name = "textBoxKullaniciAdi";
+            this.textBoxKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxKullaniciAdi.TabIndex = 4;
+            // 
+            // textBoxAdiSoyadi
+            // 
+            this.textBoxAdiSoyadi.Location = new System.Drawing.Point(79, 45);
+            this.textBoxAdiSoyadi.Name = "textBoxAdiSoyadi";
+            this.textBoxAdiSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAdiSoyadi.TabIndex = 5;
+            // 
+            // textBoxEPosta
+            // 
+            this.textBoxEPosta.Location = new System.Drawing.Point(79, 83);
+            this.textBoxEPosta.Name = "textBoxEPosta";
+            this.textBoxEPosta.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEPosta.TabIndex = 6;
+            // 
+            // textBoxSifre
+            // 
+            this.textBoxSifre.Location = new System.Drawing.Point(79, 121);
+            this.textBoxSifre.Name = "textBoxSifre";
+            this.textBoxSifre.PasswordChar = '*';
+            this.textBoxSifre.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSifre.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(79, 183);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(100, 32);
+            this.btnListele.TabIndex = 9;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.kAdiDataGridViewTextBoxColumn,
+            this.adiSoyadiDataGridViewTextBoxColumn,
+            this.ePostaDataGridViewTextBoxColumn,
+            this.sifreDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.uyelers565BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(185, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(307, 202);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.Visible = false;
+            // 
+            // uyeDb565DataSet
+            // 
+            this.uyeDb565DataSet.DataSetName = "UyeDb565DataSet";
+            this.uyeDb565DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // uyelers565BindingSource
+            // 
+            this.uyelers565BindingSource.DataMember = "Uyelers565";
+            this.uyelers565BindingSource.DataSource = this.uyeDb565DataSet;
+            // 
+            // uyelers565TableAdapter
+            // 
+            this.uyelers565TableAdapter.ClearBeforeFill = true;
+            // 
+            // kAdiDataGridViewTextBoxColumn
+            // 
+            this.kAdiDataGridViewTextBoxColumn.DataPropertyName = "KAdi";
+            this.kAdiDataGridViewTextBoxColumn.HeaderText = "KAdi";
+            this.kAdiDataGridViewTextBoxColumn.Name = "kAdiDataGridViewTextBoxColumn";
+            // 
+            // adiSoyadiDataGridViewTextBoxColumn
+            // 
+            this.adiSoyadiDataGridViewTextBoxColumn.DataPropertyName = "AdiSoyadi";
+            this.adiSoyadiDataGridViewTextBoxColumn.HeaderText = "AdiSoyadi";
+            this.adiSoyadiDataGridViewTextBoxColumn.Name = "adiSoyadiDataGridViewTextBoxColumn";
+            // 
+            // ePostaDataGridViewTextBoxColumn
+            // 
+            this.ePostaDataGridViewTextBoxColumn.DataPropertyName = "EPosta";
+            this.ePostaDataGridViewTextBoxColumn.HeaderText = "EPosta";
+            this.ePostaDataGridViewTextBoxColumn.Name = "ePostaDataGridViewTextBoxColumn";
+            // 
+            // sifreDataGridViewTextBoxColumn
+            // 
+            this.sifreDataGridViewTextBoxColumn.DataPropertyName = "Sifre";
+            this.sifreDataGridViewTextBoxColumn.HeaderText = "Sifre";
+            this.sifreDataGridViewTextBoxColumn.Name = "sifreDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxSifre);
+            this.Controls.Add(this.textBoxEPosta);
+            this.Controls.Add(this.textBoxAdiSoyadi);
+            this.Controls.Add(this.textBoxKullaniciAdi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uyeDb565DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uyelers565BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxKullaniciAdi;
+        private System.Windows.Forms.TextBox textBoxAdiSoyadi;
+        private System.Windows.Forms.TextBox textBoxEPosta;
+        private System.Windows.Forms.TextBox textBoxSifre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private UyeDb565DataSet uyeDb565DataSet;
+        private System.Windows.Forms.BindingSource uyelers565BindingSource;
+        private UyeDb565DataSetTableAdapters.Uyelers565TableAdapter uyelers565TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kAdiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adiSoyadiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ePostaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sifreDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ UygulamaSinavi2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UygulamaSinavi2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection(@"Data Source=tsomtal.com;Initial Catalog=UyeDb565;Integrated Security=True;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                string sqlCum = "INSERT INTO uyelers565 (KAdi,AdiSoyadi,EPosta,Sifre) VALUES('" + textBoxKullaniciAdi.Text + "','" + textBoxAdiSoyadi.Text + "','" + textBoxEPosta.Text + "','" + textBoxSifre.Text + "');";
+                SqlCommand cmd = new SqlCommand(sqlCum,baglanti);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Veriler başarı ile kaydedildi.");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı hatası\n"+ex. Message);
+            }
+            finally 
+            {
+                if (baglanti != null) baglanti.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'uyeDb565DataSet.Uyelers565' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.uyelers565TableAdapter.Fill(this.uyeDb565DataSet.Uyelers565);
+
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Visible=true;
+        }
+    }
+}
